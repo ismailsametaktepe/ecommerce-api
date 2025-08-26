8c13ac9420d9a3fbd19d3c6b0a674777de3bad16
İsmail Samet AKTEPE <ismail.aktepe0671@gmail.com>
2025-08-26 21:57:55 +0300

    feat: add Product entity and ProductRepository with PostgreSQL config. product entity'si ve product repositorysi ve postgre database bağlantısı yapıldı

---
+++ src/main/java/com/example/ecommerce/dataAccess/abstracts/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.ecommerce.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.ecommerce.entities.concretes.Product;
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+	
+
+}
---
+++ src/main/java/com/example/ecommerce/entities/concretes/Product.java
@@ -0,0 +1,31 @@
+package com.example.ecommerce.entities.concretes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Table(name="products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+	
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+    @Column(nullable= false)
+	private String name;
+	private double price;
+	@Column(length =32)
+	private String stockCode;
+	@Column(unique = true, length = 13)
+	private String barcode;
+	private String ticket;
+
+}
